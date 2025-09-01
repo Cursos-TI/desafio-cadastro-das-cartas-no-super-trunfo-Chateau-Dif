d4cc9f1e1fd7b10bdf2062db64f9999214dde65a
Chateau-Dif <lffguedes@yahoo.com>
2025-09-01 02:45:44 +0000

    Finalizando o cadastro da segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -30,7 +30,6 @@ float PIB1;
 float Área1;
 
 
-
 printf("Bem vindo ao Super Trunfo!\n Crie seu competidor logo abaixo!\n"); //uma mensagem de boas vindas, na minha opinião, é uma boa forma de começar
 
 printf("Digite o Estado de onde é sua Cidade: ");
@@ -39,12 +38,95 @@ scanf("%s",&Estado1);
 printf("\n Agora o nome da sua Cidade: ");
 scanf("%s",&Cidade1);
 
-printf("\n Qual a população desse local?"); 
+printf("\n Qual a população desse local? "); 
 scanf("%i",&População1);
 //já que o nome já foi dado, poderia colocar a pergunta "qual a população de (Cidade1)?"
 
-printf("\n Qual o tamanho dessa Cidade?"); 
+printf("\n Qual o tamanho dessa Cidade? "); 
 scanf("%f",&Área1);
 
+printf("\n Quantos Pontos Turísticos tem essa cidade? "); 
+scanf("%f",&Atrações1);
+
+printf("\n Qual, mais ou menos, é o produto interno bruto dessa cidade? "); 
+scanf("%f",&PIB1);
+
+
+
+
+
+char Estado2 [10];
+
+char Codigo2 [10];
+
+char Cidade2 [10];
+
+int Atrações2;
+
+int População2;
+
+float PIB2;
+
+float Área2;
+
+
+printf("\n\n\n Agora é hora de criar seu Adversário!\n" );
+
+printf("Digite o Estado de onde é sua Cidade: ");
+scanf("%s",&Estado2);
+
+printf("\n Agora o nome da sua Cidade: ");
+scanf("%s",&Cidade2);
+
+printf("\n Qual a população desse local? "); 
+scanf("%i",&População2);
+
+printf("\n Qual o tamanho dessa Cidade? "); 
+scanf("%f",&Área2);
+
+printf("\n Quantos Pontos Turísticos tem essa cidade? "); 
+scanf("%f",&Atrações2);
+
+printf("\n Qual, mais ou menos, é o produto interno bruto dessa cidade? "); 
+scanf("%f",&PIB2);
+
+
+printf("\n\n Agora que as cartas estão cadastradas, vamos a suas características!\n");
+
+printf("\n Carta Número 1 (Código C1)\n\n");
+
+printf("   Nome: %s\n", Cidade1);
+
+printf("   Código: C1\n");
+
+printf("   Estado: %s\n", Estado1);
+
+printf("   População: %i\n", População1);
+
+printf("   Área: %f\n", Área1);
+
+printf("   Pontos Turísticos: %f\n", Atrações1);
+
+printf("   PIB: %f\n\n\n\n\n", PIB1);
+
+
+
+printf("\n Carta Número 1 (Código C2)\n\n");
+
+printf("   Nome: %s\n", Cidade1);
+
+printf("   Código: C2\n");
+
+printf("   Estado: %s\n", Estado2);
+
+printf("   População: %i\n", População2);
+
+printf("   Área: %f\n", Área2);
+
+printf("   Pontos Turísticos: %f\n", Atrações2);
+
+printf("   PIB: %f\n\n\n", PIB2);
+
     return 0;
+
 }
